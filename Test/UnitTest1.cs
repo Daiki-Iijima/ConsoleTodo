586cfb79b147ac0cf4613edb28ba27bad7921e15
daiki <daiki@ROG_ZEPHYRUS>
2023-04-13 17:52:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Todoオブジェクトを操作する_CRUDクラス {
+    public class Todo_Test{
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Fail("失敗");
+        }
+    }
+}
