f945989ac8d3728069b601713f4dd5f9240eb055
Daiki Iijima <daiki.applecreate@gmail.com>
2023-04-14 12:01:48 +0900

    [add] 不安だったので同系列のテストを追加

--- Test/TodoTest.cs
+++ Test/TodoTest.cs
@@ -14,5 +14,14 @@ namespace Todoオブジェクトを操作する_CRUDクラス {
             //  検証
             Assert.AreEqual(new TodoTask("test"), task);
         }
+
+        [Test]
+        public void _1234Tを入力すると_1234Tを持ったTodoを生成する() {
+            //  準備&実行
+            TodoTask task = new TodoTask("_1234T");
+
+            //  検証
+            Assert.AreEqual(new TodoTask("_1234T"), task);
+        }
     }
 }
