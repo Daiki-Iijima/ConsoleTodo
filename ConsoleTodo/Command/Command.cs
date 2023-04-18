73c97cf0d23e1f5ca3ee81561ec8e62666542ed9
Daiki Iijima <daiki.applecreate@gmail.com>
2023-04-18 11:10:19 +0900

    [fix] とりあえず処理をクラスに括りだした

--- ConsoleTodo/Command/Command.cs
+++ ConsoleTodo/Command/Command.cs
@@ -10,23 +10,20 @@ namespace ConsoleTodo.Command {
         public Action<int> RemoveEvenet { get; set; }
 
         public bool CheckCommand(string commandStr) {
-            if (commandStr.Contains("add")) {
-                string[] commandAry = commandStr.Split(' ');
-                AddEvent?.Invoke(commandAry[1]);
-                return true;
+            AddCommand addCommand = new AddCommand("add", AddEvent);
+            RemoveCommand removeCommand = new RemoveCommand("remove", RemoveEvenet);
+
+            bool commandResult = false;
+
+            if (addCommand.Check(commandStr)) {
+                commandResult = addCommand.Execute();
             }
 
-            if (commandStr.Contains("remove")) {
-                string[] commandAry = commandStr.Split(' ');
-                if (int.TryParse(commandAry[1], out int i)) {
-                    RemoveEvenet?.Invoke(i);
-                    return true;
-                } else {
-                    return false;
-                }
+            if (removeCommand.Check(commandStr)) {
+                commandResult = removeCommand.Execute();
             }
 
-            return false;
+            return commandResult;
         }
     }
 }
---
+++ ConsoleTodo/Command/RemoveCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTodo.Command {
+    public class RemoveCommand {
+        public string WakeWord { get; private set; }
+        public Action<int> ExcuteAction { get; set; }
+
+        private List<string> arg = new List<string>();
+
+        public RemoveCommand(string wakeWord, Action<int> excuteAction) {
+            WakeWord = wakeWord;
+            ExcuteAction = excuteAction;
+        }
+
+        public bool Check(string command) {
+            string[] commandAry = command.Split(' ');
+            if (commandAry[0].Contains(WakeWord)) {
+                arg = commandAry.ToList().GetRange(1, commandAry.Length - 1);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public bool Execute() {
+            if (int.TryParse(arg[0], out int parseInt)) {
+                ExcuteAction?.Invoke(parseInt);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
